462ee62e2bda3fd5e3163eb23095ba207a578158
Txomin Medrano Martorell <tmedrano@tarragona.cat>
2019-11-12 14:37:31 +0100

    isjson function

--- src/Helpers/censat.php
+++ src/Helpers/censat.php
@@ -5,3 +5,14 @@ if (! function_exists('censat')) {
 		return new \Ajtarragona\Censat\Models\CensatClient($options);
 	}
 }
+
+if (! function_exists('isJson')) {
+	function isJson($string) {
+	 	try{
+	 		json_decode($string);
+			return (json_last_error() == JSON_ERROR_NONE);
+		}catch(Exception $e){
+			return false;
+		}
+	}
+}
